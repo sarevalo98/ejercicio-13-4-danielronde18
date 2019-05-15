31db1d2051e8bcace122de735b4f01f437687104
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2019-05-15 18:32:41 -0500

    Add files via upload

---
+++ bifurcaima.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pylab as plt
+import math
+
+datos=np.loadtxt("datosbifur.dat", delimiter="	" )
+datos_t= datos[:,0]
+datos_y= datos[:,1]
+
+plt.figure()
+plt.plot(datos_t,datos_y)
+plt.show()
